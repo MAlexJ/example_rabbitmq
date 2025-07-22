967c119819c443f27fcc0879ecfc951577b513a1
malexj <alexmaximovj@gmail.com>
2025-07-22 13:54:23 +0300

    Add samples for headers consumers

---
+++ exchange_types/headers/headers_consumer_pdf/src/main/java/example/java/headers_consumer_pdf/HeadersConsumerPdfApplication.java
@@ -0,0 +1,12 @@
+package example.java.headers_consumer_pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadersConsumerPdfApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeadersConsumerPdfApplication.class, args);
+  }
+}
---
+++ exchange_types/headers/headers_consumer_txt/src/main/java/example/java/headers_consumer_txt/HeadersConsumerTxtApplication.java
@@ -0,0 +1,12 @@
+package example.java.headers_consumer_txt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadersConsumerTxtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HeadersConsumerTxtApplication.class, args);
+  }
+}
